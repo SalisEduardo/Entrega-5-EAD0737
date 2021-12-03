4ac2c6a2a9aecb0be700fa23df31e5d1d8da3de1
dcardosos <douglascardoso@usp.br>
2021-12-03 15:31:29 -0300

    melhorado

--- mpt.R
+++ mpt.R
@@ -18,219 +18,209 @@
 #Pacotes
 
 # library(quantmod)   # nao foi necessaria
-library(readxl)
-library(xts)
+library(magrittr)
 library(PortfolioAnalytics)
 library(PerformanceAnalytics)
+
+library(readxl)
+library(xts)
 library(ROI.plugin.quadprog)
 library(ROI.plugin.glpk)
 library(tidyverse)
 library(corrplot)
 library(quantmod)   
+library(corrr)
 
-# ------------------------------------------------------------------------------
-
-
-#Seleção do período de análise  
+# Captura dos dados -------------------------------------------------------
+  
 startDate = as.Date("2019-01-01")   
 endDate = as.Date("2021-11-30")  
 
-#Captura dos dados  
+tickers <- c("PRIO3.SA", "ITUB4.SA", "JPSA3.SA", "ARZZ3.SA", "ENGI11.SA", 
+             "SLCE3.SA", "WEGE3.SA", "GGBR4.SA", "PSSA3.SA")
 
-getSymbols("PRIO3.SA" , src = "yahoo", from = startDate, to = endDate)  
-#getSymbols("RAIZ4.SA" , src = "yahoo", from = startDate, to = endDate)  
-getSymbols("ITUB4.SA" , src = "yahoo", from = startDate, to = endDate)  
-getSymbols("JPSA3.SA" , src = "yahoo", from = startDate, to = endDate)
-getSymbols("ARZZ3.SA" , src = "yahoo", from = startDate, to = endDate)
-getSymbols("ENGI11.SA", src = "yahoo", from = startDate, to = endDate)
-getSymbols("SLCE3.SA" , src = "yahoo", from = startDate, to = endDate)
-getSymbols("WEGE3.SA" , src = "yahoo", from = startDate, to = endDate)
-getSymbols("GGBR4.SA" , src = "yahoo", from = startDate, to = endDate)
-getSymbols("PSSA3.SA" , src = "yahoo", from = startDate, to = endDate)
+quantmod::getSymbols(tickers , src = "yahoo", from = startDate, to = endDate)  
 
-#Cálculo dos retornos  
+# Cálculo dos retornos  ---------------------------------------------------
+extract_name <- function(df){
+  
+  df %>% 
+    names() %>% 
+    purrr::pluck(1) %>% 
+    stringr::str_extract(".+(?=.SA.Open)")
+}
 
-PRIO3_RET  <- dailyReturn(PRIO3.SA)  
-#RAIZ4_RET  <- dailyReturn(RAIZ4.SA)  
-ITUB4_RET  <- dailyReturn(ITUB4.SA) 
-JPSA3_RET  <- dailyReturn(JPSA3.SA) 
-ARZZ3_RET  <- dailyReturn(ARZZ3.SA) 
-ENGI11_RET <- dailyReturn(ENGI11.SA) 
-SLCE3_RET <- dailyReturn(SLCE3.SA) 
-WEGE3_RET <- dailyReturn(WEGE3.SA) 
-GGBR4_RET <- dailyReturn(GGBR4.SA) 
-PSSA3_RET <- dailyReturn(PSSA3.SA) 
+daily_return <- function(tk){
+  
+  quantmod::dailyReturn(tk) %>% 
+    broom::tidy() %>% 
+    dplyr::mutate(ticker = extract_name(tk)) %>% 
+    dplyr::select(-series)
+}
 
-#retornos <- as.data.frame(merge(PRIO3_RET, RAIZ4_RET, ITUB4_RET, JPSA3_RET, ARZZ3_RET, ENGI11_RET, SLCE3_RET, WEGE3_RET, GGBR4_RET, PSSA3_RET))
-retornos <- as.data.frame(merge(PRIO3_RET, ITUB4_RET, JPSA3_RET, ARZZ3_RET, ENGI11_RET, SLCE3_RET, WEGE3_RET, GGBR4_RET, PSSA3_RET))
+data_list <- list(PRIO3.SA, ITUB4.SA, JPSA3.SA, ARZZ3.SA, ENGI11.SA, 
+                  SLCE3.SA, WEGE3.SA, GGBR4.SA, PSSA3.SA)
 
+tab_ret <- purrr::map_dfr(data_list, daily_return)
 
-#tickers <- c("PRIO3.SA", "RAIZ4.SA", "ITUB4.SA", "JPSA3.SA","ARZZ3.SA", "ENGI11.SA" , "SLCE3.SA", "WEGE3.SA","GGBR4.SA","PSSA3.SA")
+returns <- 
+  tab_ret %>% 
+  tidyr::pivot_wider(
+    names_from = ticker,
+    values_from = value
+  ) %>% 
+  dplyr::rename(date = index)
 
-tickers <- c("PRIO3.SA", "ITUB4.SA", "JPSA3.SA","ARZZ3.SA", "ENGI11.SA" , "SLCE3.SA", "WEGE3.SA","GGBR4.SA","PSSA3.SA")
+returns
 
+# Avaliar as correlacoes dos retornos -------------------------------------
+m_corrr <- returns %>% 
+  dplyr::select(-date) %>% 
+  corrr::correlate() 
 
+m_corrr %>% 
+  corrr::rearrange() %>% 
+  corrr::shave() %>% 
+  corrr::rplot(colours = "darkred")
 
-colnames(retornos) <- tickers
+# mudando o formato do dado
+tidy_corrr <- m_corrr %>% 
+  tidyr::pivot_longer(
+    cols = PRIO3:PSSA3,
+    names_to = "tk_2",
+    values_to = "corr"
+  ) %>% 
+  dplyr::rename(tk_1 = term)
+  
+# incerteza sobre ser uma boa metrica
+tidy_corrr %>% 
+  dplyr::summarise(corr_media = mean(corr, na.rm = TRUE))
 
-retornos
-# ------------------------------------------------
+# pares de acoes com alta correlacao
+tidy_corrr %>% 
+  dplyr::filter(abs(corr) >= 0.5 & abs(corr) <= 1)
 
-# Avaliar as correlacoes dos retornos:
+# Montando a carteira -----------------------------------------------------
 
-noAcoes = ncol(retornos) 
+# divisão da amostra
 
-matrizCorrelacao = round(cor(as.matrix(retornos),use = "pairwise.complete.obs"),3)
+amostra <- returns %>% dplyr::filter(date <= "2021-10-29")
+fora_amostra <- returns %>% dplyr::anti_join(amostra, by = "date")
 
-corrplot(matrizCorrelacao) # plot matriz de correlação
+# estatisticas descritivas retornos
+summary(amostra) 
 
-palette = colorRampPalette(c("green", "white", "red")) (20)
-heatmap(x = matrizCorrelacao, col = palette, symm = TRUE)
-
-correlacao_media <- mean(matrizCorrelacao) # incerteza sobre ser uma boa metrica
-
-n_pares_acoes_alta_corr <-sum(abs(as.data.frame(matrizCorrelacao))>= 0.5 & abs(as.data.frame(matrizCorrelacao)) < 1)
-
-corr_df <- as.data.frame(matrizCorrelacao)
-
-# selecionando altas correlacoes
-corr_df[abs(corr_df) < 0.6 | abs(corr_df) == 1] <- "" 
-
-corr_df
-
-## outra forma
-
-index <- which(abs(matrizCorrelacao) > 0.6 & abs(matrizCorrelacao) < 1,
-               arr.ind = T) 
-cbind.data.frame(stock1 = rownames(matrizCorrelacao)[index[,1]], 
-                 stock2 = colnames(matrizCorrelacao)[index[,2]]) 
-
-
-# ------------------------------------------------
-
-
-## split_dados <- round(0.8 * nrow(retornos))
-##  amostra = retornos[1:split_dados,] # divisao com base nos dados
-##  fora_amostra = retornos[split_dados:nrow(retornos),]
-
-# Periodo de teste de sesempenho da carteira e o mes de novembro
-
-amostra <- retornos[which(row.names(retornos) == "2019-01-02") : which(row.names(retornos) == "2019-10-29"), , drop = FALSE]
-fora_amostra <- retornos[which(row.names(retornos) == "2021-11-01") : which(row.names(retornos) == "2021-11-29"), , drop = FALSE]
-
-summary(amostra) # estatisticas descritivas retornos
-
-
-# Especificacoes da carteira:
-
-fund.names = colnames(retornos) # nome dos ativos
-
-carteira = portfolio.spec(assets = fund.names) # criando a carteira
+# especificacoes da carteira:
+carteira_begin <- portfolio.spec(assets = tickers) 
 
 # Restricao 1 - carteira totalmente investida:
-
-carteira = add.constraint(portfolio = carteira, type = "full_investment")
+carteira_full_investment <- add.constraint(portfolio = carteira_begin, 
+                                          type = "full_investment")
 
 # Restricao 2 - apenas posicoes compradas:
-
-carteira = add.constraint(portfolio = carteira, type = "long_only")
+carteira_long_only <- add.constraint(portfolio = carteira_full_investment, 
+                          type = "long_only")
 
 # Restricao 3 - para os pesos:
+carteira_box = add.constraint(portfolio = carteira_long_only, 
+                              type = "box", 
+                              min = 0.01, max = 0.2)
 
-carteira = add.constraint(portfolio = carteira, type = "box", min = 0.01, max = 0.2)
+# criando um xts dos retornos
+xts_amostra <- amostra %>% timetk::tk_xts()
+
+# primeiro jeito
+ef_mean_var <- create.EfficientFrontier(R = xts_amostra,
+                                        portfolio = carteira_box,
+                                        type = "mean-var",
+                                        n.portfolios = 100,
+                                        )
+
+chart.EfficientFrontier(ef_mean_var,
+                        match.col = "StdDev")
+
+# segundo jeito
+FE <- meanvar.efficient.frontier(portfolio = carteira_box, 
+                                 xts_amostra, 
+                                 n.portfolios = 100)
+plot(100*FE[,2],
+     100*FE[,1],
+     xlab="Risco (%)",
+     ylab = "Retorno (%)",
+     col="blue",
+     main = "Fronteira Eficiente")
 
 
-FE = meanvar.efficient.frontier(portfolio = carteira, retornos, n.portfolios = 100)
-plot(100*FE[,2],100*FE[,1],xlab="Risco (%)",ylab = "Retorno (%)",col="blue",main = "Fronteira Eficiente")
-
-# ----------------------------------------------------------------------------------------------------------------
-
-# Processo de otimizacao... 
-
-# Definindo o objetivo do investidor:
+# Processo de otimizacao --------------------------------------------------
 
 # 1. Carteira de variancia minima (CVM) - eficiente e com menor risco...
-
-carteira = add.objective(portfolio = carteira, type = "risk", name = "StdDev")
+carteira_cvm <- add.objective(portfolio = carteira_box, type = "risk", name = "StdDev")
 
 # 2. Carteira de retorno pre definido - eficiente e com menor risco para o retorno desejado...
-
 # carteira <- add.constraint(portfolio = carteira, type = "return",return_target = 0.0008)
 
-# Otimizando a carteira...
+my_carteira <- optimize.portfolio(R = xts_amostra,
+                                  portfolio = carteira_cvm,
+                                  optimize_method = "ROI",
+                                  trace = TRUE)
 
-MinhaCarteira = optimize.portfolio(R = retornos,portfolio = carteira,optimize_method = "ROI",trace = TRUE)
+my_carteira 
 
-MinhaCarteira # informacoes da carteira
+# Retorno medio (%) da carteira:
+Return.portfolio(xts_amostra, weights = extractWeights(my_carteira)) %>% 
+  mean() * 100
 
-# Calcular o retorno medio (%) da carteira:
-
-retorno_esperado_amostra <- mean(Return.portfolio(retornos,weights = extractWeights(MinhaCarteira)))*100
-retorno_esperado_amostra
-
-# Verifique a carteira na fronteira eficiente!!!
-
-100*round(MinhaCarteira[["weights"]],4) # pesos em cada ativo (%)
-
+# A carteira está fronteira eficiente?
+round(my_carteira[["weights"]], 4) * 100 # pesos em cada ativo (%)
 
 # Alocacoes:
+plot(my_carteira)
 
-plot(MinhaCarteira)
+# Teste fora da amostra ---------------------------------------------------
 
-# Vamos verificar o desempenho dela fora da amostra 
+# Retornos nesse periodo:
+xts_fora_mostra <- fora_amostra %>% timetk::tk_xts()
 
-# Calcular os retornos nesse periodo:
+test_returns = Return.portfolio(xts_fora_mostra,
+                             weights = extractWeights(my_carteira))
 
-RetornoMC = Return.portfolio(fora_amostra,weights = extractWeights(MinhaCarteira))
 
-# Retorno medio (%) fora da amostra:
-
-retorno_medio_teste_MC <- mean(RetornoMC)*100
-
-# Desvio-padrao (%) fora da amostra:
-
-sd_retorno_test_MC  <- sd(RetornoMC)*100
-
-# Sharpe (simplificando a analisa para rf = 0)
-sharpe_teste_MC <- retorno_medio_teste_MC/sd_retorno_test_MC
-
-# Vizualizacao:
-
-plot(RetornoMC)
-
-# Visualizar retornos acumulados (soma geometrica dos retornos dia a dia): 
-
-chart.CumReturns(RetornoMC)
-
-# --------------------------------------------------------------------------------------------------------
-# comparacao com benchmark de pesos iguais
-
-Retorno_Carteira_EW = Return.portfolio(fora_amostra,weights = carteira$assets)
-
-# Retorno medio EW (%) fora da amostra:
-
-retorno_medio_teste_EW <- mean(Retorno_Carteira_EW)*100
-
-# Desvio-padrao EW (%) fora da amostra:
-
-sd_retorno_test_EW  <- sd(Retorno_Carteira_EW)*100
-
-# Sharpe (simplificando a analisa para rf = 0)
-sharpe_teste_EW <- retorno_medio_teste_EW/sd_retorno_test_EW
+test_returns %>% 
+  broom::tidy() %>% 
+  dplyr::select(-series) %>% 
+  dplyr::rename(date = index) %>% 
+  dplyr::summarise(
+    test_mean_return = mean(value) * 100,
+    test_sd_return = sd(value) * 100,
+    test_sharp = test_mean_return / test_sd_return
+  )
 
 # Vizualizacao:
 
-plot(Retorno_Carteira_EW)
+plot(test_returns, col = "darkred", 
+     main = "Retornos fora da amostra")
 
 # Visualizar retornos acumulados (soma geometrica dos retornos dia a dia): 
 
-chart.CumReturns(Retorno_Carteira_EW)
+chart.CumReturns(test_returns)
 
+# Comparação --------------------------------------------------------------
 
+ew_returns <- Return.portfolio(xts_fora_mostra, 
+                               weights = carteira_cvm$assets) 
+  
+ew_returns %>% 
+  broom::tidy() %>% 
+  dplyr::select(-series) %>% 
+  dplyr::rename(date = index) %>% 
+  dplyr::summarise(
+    ew_mean_return = mean(value) * 100,
+    ew_sd_return = sd(value) * 100,
+    ew_sharp = ew_mean_return / ew_sd_return
+  )
 
+# Vizualizacao:
+plot(ew_returns)
 
-
-
-
-
-
+# Visualizar retornos acumulados (soma geometrica dos retornos dia a dia): 
+chart.CumReturns(ew_returns)
